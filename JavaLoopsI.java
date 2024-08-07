d01c29d6adc3194d608063a303cae18c96acbe3d
VegiSujini <sujini.vegi@gmail.com>
2024-08-07 22:01:04 +0530

    added java loop file

---
+++ JavaLoopsI.java
@@ -0,0 +1,28 @@
+
+// Problem Name :
+
+// Java Loops I
+
+// Problem URL :
+
+// https://www.hackerrank.com/challenges/java-loops-i/problem?isFullScreen=true
+
+// Solution File :
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaLoopsI {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        // for(int i=1;i<N;i++){
+        for (int j = 1; j <= 10; j++) {
+            System.out.println(N + " x " + j + " = " + N * j);
+            // }
+        }
+        bufferedReader.close();
+    }
+}
