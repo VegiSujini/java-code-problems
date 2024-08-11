aebc4acf783fa4de77751a4b8a445d20cc4607bb
VegiSujini <sujini.vegi@gmail.com>
2024-08-11 10:36:31 +0530

    modified java string introduction file

--- JavaStringsIntroduction.java
+++ JavaStringsIntroduction.java
@@ -8,25 +8,39 @@
 
 // Solution File :
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class JavaStringsIntroduction {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
-        if (N == 1 || N == 5 || N % 2 != 0) {
-            System.out.print("Weird");
-        } else if (N % 2 == 0) {
-            if (N == 0 || 2 >= N || N < 5)
-                System.out.print("Not Weird");
-            else if (6 >= N || N <= 20)
-                System.out.print("Weird");
-            else if (N > 20)
-                System.out.print("Not Weird");
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        int len1 = A.length();
+        int len2 = B.length();
+        int sumt = len1 + len2;
+        System.out.println(sumt);
+        if (A.charAt(0) > B.charAt(0)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
         }
-        bufferedReader.close();
+        char nstr[] = new char[sumt + 1];
+        int j = 1;
+        for (int i = 0; i <= sumt; i++) {
+            if (i == 0) {
+                nstr[i] = Character.toUpperCase(A.charAt(0));
+            } else {
+                if (i == len1) {
+                    nstr[i] = ' ';
+                } else if (i == len1 + 1) {
+                    nstr[i] = Character.toUpperCase(B.charAt(0));
+                } else if (i < len1)
+                    nstr[i] = A.charAt(i);
+                else
+                    nstr[i] = B.charAt(j++);
+            }
+            System.out.print(nstr[i]);
+        }
     }
 }
