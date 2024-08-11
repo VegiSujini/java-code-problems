2e0fd1dc849f71465d06feca8c93dbcf8d65a6c1
VegiSujini <sujini.vegi@gmail.com>
2024-08-11 08:23:38 +0530

    modified java substring file

--- JavaSubstring.java
+++ JavaSubstring.java
@@ -1,3 +1,41 @@
-public class JavaSubstring {
-    
+// Problem Name :
+
+// Java String Reverse
+
+// Problem URL :
+
+// https://www.hackerrank.com/challenges/java-string-reverse/problem?isFullScreen=true
+
+// Solution File :
+import java.util.Scanner;
+
+class JavaSubstring {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+
+        int n = A.length();
+        char oa[] = new char[n];
+        char ra[] = new char[n];
+        for (int i = 0; i < n; i++) {
+            oa[i] = A.charAt(i);
+        }
+        for (int i = n - 1, k = 0; i >= 0; i--) {
+
+            ra[k++] = A.charAt(i);
+        }
+
+        int l = 0;
+        for (int i = 0; i < n; i++) {
+            if (oa[i] == ra[i]) {
+                l++;
+            }
+        }
+        if (l == n)
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+    }
 }
